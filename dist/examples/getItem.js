9a41c13c9e87b9252b591321cdbccef9f6a76880
Mark Fulton <mark.fulton@workgrid.com>
2019-12-19 16:38:08 +0000

    master - Removed dist

--- dist/examples/getItem.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var index_1 = require("../index");
-var builder = new index_1.ConstructGet('myTable');
-var query = builder
-    .setKeyCondition('myKey', 55)
-    .setKeyCondition('anotherKey', 'stringType')
-    .getConstructedQuery();
-console.log(query);
--- dist/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BuilderType;
-(function (BuilderType) {
-    BuilderType[BuilderType["DynamoDB"] = 0] = "DynamoDB";
-    BuilderType[BuilderType["DocumentClient"] = 1] = "DocumentClient";
-})(BuilderType = exports.BuilderType || (exports.BuilderType = {}));
